1689d4cb0d6bc9acd0dbb47e1aece7efdf24dca8
DESKTOP-SO1RK4N\jannu <jannunzi@gmail.com>
2021-05-27 23:39:16 -0400

    added Welcome message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Poly Docs
+          Welcome to Poly Docs
         </p>
         <a
           className="App-link"
